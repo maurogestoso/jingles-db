ab3b2456c4c3ae0c33a52515559781d94ea56634
Mauro Gestoso <maurogestoso@gmail.com>
2025-03-19 00:05:46 +0100

    Add jingleeeees animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,46 @@
+import { useRef, useState } from "react";
 import { Outlet } from "react-router";
 
 export default function Layout() {
+  const { eees, onMouseEnter, onMouseLeave } = useJingleeesAnimation();
   return (
     <>
       <header className="p-4 flex justify-center">
-        <h1 className="text-4xl font-bold mb-6">Jingleeeeeees DB</h1>
+        <h1
+          className="text-4xl font-bold mb-6"
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          Jingl{eees}s! DB
+        </h1>
       </header>
       <main className="max-w-screen-sm mx-auto p-4">
         <Outlet />
       </main>
     </>
   );
+}
+
+function useJingleeesAnimation() {
+  const interval = useRef<NodeJS.Timeout | null>(null);
+  const [eees, setEees] = useState("e");
+  return {
+    eees,
+    onMouseEnter: () => {
+      clearInterval(interval.current!);
+      interval.current = setInterval(() => {
+        setEees((eees) => eees + "e");
+      }, 100);
+    },
+    onMouseLeave: () => {
+      clearInterval(interval.current!);
+      interval.current = setInterval(() => {
+        if (eees.length === 1) {
+          clearInterval(interval.current!);
+          return;
+        }
+        setEees((eees) => (eees.length > 1 ? eees.slice(0, -1) : "e"));
+      }, 50);
+    },
+  };
 }
