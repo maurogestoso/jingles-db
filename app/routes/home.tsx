abc982d977b71b6266627ab7c469017a9ce451de
Mauro Gestoso <maurogestoso@gmail.com>
2025-03-18 21:36:20 +0100

    Remove unused code for recent jingles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,4 @@
-import { db } from "~/db";
 import type { Route } from "./+types/home";
-import { artists, authors, episodes, songs } from "~/db/schema";
-import { eq, sql, desc } from "drizzle-orm";
-import JinglesTable, { columns } from "~/components/jingles-table";
 import { Form } from "react-router";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
@@ -17,27 +13,7 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader() {
-  const jingles = await db
-    .select({
-      id: songs.id,
-      name: songs.name,
-      artist: artists.name,
-      author: authors.name,
-      youtubeUrl: sql<string>`CONCAT(${episodes.youtubeUrl}, '&t=', ${songs.timestamp}, 's')`,
-    })
-    .from(songs)
-    .innerJoin(artists, eq(songs.artistId, artists.id))
-    .innerJoin(episodes, eq(songs.episodeId, episodes.id))
-    .leftJoin(authors, eq(songs.authorId, authors.id))
-    .orderBy(desc(songs.createdAt))
-    .limit(10);
-
-  return { jingles };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  const { jingles } = loaderData;
+export default function Home({}: Route.ComponentProps) {
   return (
     <>
       <Form method="get" action="/search" className="mb-4">
