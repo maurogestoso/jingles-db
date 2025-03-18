55cacfa6e713a2a3bcb1b291ef5c94861aa3265a
Mauro Gestoso <maurogestoso@gmail.com>
2025-03-18 21:35:21 +0100

    Remove recent jingles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -51,9 +51,6 @@ export default function Home({ loaderData }: Route.ComponentProps) {
           <Button type="submit">Buscar</Button>
         </div>
       </Form>
-
-      <h2 className="text-3xl font-bold mb-4">Jingles mas recientes</h2>
-      <JinglesTable columns={columns} data={jingles} />
     </>
   );
 }
