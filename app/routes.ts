639d3e73a07114020ddaa6e1fbe0e9833169dec4
Mauro Gestoso <maurogestoso@gmail.com>
2025-05-09 23:57:59 +0200

    Add tags-detail page

--- app/routes.ts
+++ app/routes.ts
@@ -9,12 +9,17 @@ export default [
   layout("layout.tsx", [
     index("routes/home.tsx"),
     route("/que-es-esto", "routes/about.tsx"),
+
     route("/programas", "routes/episodes-index.tsx"),
     route("/programas/:id", "routes/episodes-detail.tsx"),
+
     route("/jingleros", "routes/authors-index.tsx"),
     route("/jingleros/:id", "routes/authors-detail.tsx"),
+
     route("/artistas", "routes/artists-index.tsx"),
     route("/artistas/:id", "routes/artists-detail.tsx"),
+
     route("/tags", "routes/tags-index.tsx"),
+    route("/tags/:id", "routes/tags-detail.tsx"),
   ]),
 ] satisfies RouteConfig;
