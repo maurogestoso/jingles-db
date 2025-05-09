e2ca641f3f01aa6afa2410f6cb6d97338e4c158a
Mauro Gestoso <maurogestoso@gmail.com>
2025-05-09 12:10:44 +0200

    Add strip accents util

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function stripAccents(word: string) {
+  return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
